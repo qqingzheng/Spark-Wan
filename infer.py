cd7208e2a28cba6803dc5a3e14c737793570b619
Shenghai Yuan <140951558+SHYuanBest@users.noreply.github.com>
2025-08-01 13:31:11 +0800

    refactor

--- infer.py
+++ infer.py
@@ -3,6 +3,7 @@ import sys
 
 sys.path.append(".")
 
+import argparse
 import os
 
 import torch
@@ -16,7 +17,7 @@ from spark_wan.training_utils.load_model import replace_rmsnorm_with_fp32
 from tqdm import tqdm
 
 from diffusers import AutoencoderKLWan
-from diffusers.schedulers import FlowMatchEulerDiscreteScheduler
+from diffusers.schedulers import FlowMatchEulerDiscreteScheduler, UniPCMultistepScheduler
 from diffusers.utils import export_to_video
 
 
@@ -27,102 +28,162 @@ dist.init_process_group(backend="nccl")
 torch.cuda.set_device(dist.get_rank())
 
 
-weight_dtype = torch.bfloat16
-seed = 2002
-flow_shift = 5.0
-guidance_scale = 5.0
-height = (720,)
-width = (1280,)
-num_inference_steps = (30,)
-num_frames = 81
+def parse_args():
+    parser = argparse.ArgumentParser(description="Generate video with model")
 
-output_path = "output"
-model_path = "/mnt/workspace/checkpoints/Wan-AI/Wan2.1-T2V-14B-Diffusers/"
-lora_path = None
-# lora_path = f"/data/pfs/checkpoints/chestnutlzj/Spark-Wan-16steps/model.safetensors"
-
-prompts = [
-    "A stylish woman walks down a Tokyo street filled with warm glowing neon and animated city signage. She wears a black leather jacket, along red dress, and black boots, and carries a black purse. She wears sunglasses and red lipstick. She walks confidently and casually. The street is dampand reflective, creating a mirror effect of thecolorful lights. Many pedestrians walk about."
-]
-negative_prompt = "Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards"
-
-
-sp_group_index, sp_group_local_rank = init_sequence_parallel_group(8)
-
-os.makedirs(output_path, exist_ok=True)
-vae = AutoencoderKLWan.from_pretrained(
-    model_path,
-    subfolder="vae",
-    torch_dtype=torch.float32,
-)
-transformer = WanTransformer3DModel.from_pretrained(
-    model_path,
-    subfolder="transformer",
-    torch_dtype=weight_dtype,
-)
-transformer = replace_rmsnorm_with_fp32(transformer)
-
-if lora_path is not None:
-    lora_target_modules = [
-        "to_q",
-        "to_k",
-        "to_v",
-        "to_out.0",
-        "proj_out",
-        "ffn.net.0.proj",
-        "ffn.net.2",
-    ]
-    lora_config = LoraConfig(r=64, lora_alpha=128, target_modules=lora_target_modules)
-    transformer = get_peft_model(transformer, lora_config)
-    state_dict = load_file(
-        lora_path,
-        device="cpu",
+    # Arguments for model parameters
+    parser.add_argument(
+        "--weight_dtype",
+        type=str,
+        default="bf16",
+        choices=["bf16", "fp16", "fp32"],
+        help="Data type for model weights.",
     )
-    missing_keys, unexpected_keys = transformer.load_state_dict(state_dict, strict=False)
-    print("unexpected_keys: ", unexpected_keys)
-    print("loading lora done!")
+    parser.add_argument("--seed", type=int, default=2002, help="Seed for random number generator.")
+    parser.add_argument("--flow_shift", type=float, default=5.0, help="Flow shift parameter.")
+    parser.add_argument("--guidance_scale", type=float, default=5.0, help="Guidance scale for model inference.")
 
-transformer.eval()
+    # Arguments for resolution
+    parser.add_argument("--height", type=int, default=720, help="Height of output frames.")
+    parser.add_argument("--width", type=int, default=1280, help="Width of output frames.")
 
-# scheduler = UniPCMultistepScheduler(
-#     prediction_type="flow_prediction",
-#     use_flow_sigmas=True,
-#     num_train_timesteps=1000,
-#     flow_shift=flow_shift,
-# )
-scheduler = FlowMatchEulerDiscreteScheduler(
-    num_train_timesteps=1000,
-    shift=flow_shift,
-)
+    # Arguments for inference steps and frames
+    parser.add_argument(
+        "--scheduler_type",
+        type=str,
+        default="euler",
+        choices=["unipc", "euler"],
+    )
+    parser.add_argument("--num_inference_steps", type=int, default=30, help="Number of inference steps.")
+    parser.add_argument("--num_frames", type=int, default=81, help="Number of frames to generate.")
 
-pipe = WanPipeline.from_pretrained(
-    model_path,
-    transformer=transformer,
-    vae=vae,
-    scheduler=scheduler,
-    torch_dtype=weight_dtype,
-)
+    # Arguments for paths
+    parser.add_argument("--output_path", type=str, default="output", help="Directory to save output.")
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        default="/mnt/workspace/checkpoints/Wan-AI/Wan2.1-T2V-14B-Diffusers/",
+        help="Path to model directory.",
+    )
+    parser.add_argument(
+        "--lora_path", type=str, default=None, help="Path to lora model (optional)."
+    )  # "/data/pfs/checkpoints/chestnutlzj/Spark-Wan-16steps/model.safetensors"
 
-pipe = pipe.to(weight_dtype, device="cuda")
+    # Arguments for prompts
+    parser.add_argument(
+        "--prompts",
+        type=str,
+        nargs="+",
+        default=[
+            "A stylish woman walks down a Tokyo street filled with warm glowing neon and animated city signage. She wears a black leather jacket, along red dress, and black boots, and carries a black purse. She wears sunglasses and red lipstick. She walks confidently and casually. The street is dampand reflective, creating a mirror effect of the colorful lights. Many pedestrians walk about."
+        ],
+        help="List of prompts for model generation.",
+    )
 
-for prompt in tqdm(prompts):
-    generator = torch.Generator(device="cuda").manual_seed(seed)
-    with torch.amp.autocast("cuda", dtype=torch.bfloat16):
-        output = pipe(
-            prompt=prompt,
-            negative_prompt=negative_prompt,
-            height=height,
-            width=width,
-            num_inference_steps=num_inference_steps,
-            num_frames=num_frames,
-            guidance_scale=guidance_scale,
-            generator=generator,
-            is_flash_attn=True,
-        ).frames[0]
-    if dist.get_rank() == 0:
-        file_count = len([f for f in os.listdir(output_path) if os.path.isfile(os.path.join(output_path, f))])
-        video_path = f"{output_path}/{seed}_{file_count:04d}.mp4"
-        export_to_video(output, video_path, fps=16)
+    parser.add_argument(
+        "--negative_prompt",
+        type=str,
+        default="Bright tones, overexposed, static, blurred details, subtitles, style, works, paintings, images, static, overall gray, worst quality, low quality, JPEG compression residue, ugly, incomplete, extra fingers, poorly drawn hands, poorly drawn faces, deformed, disfigured, misshapen limbs, fused fingers, still picture, messy background, three legs, many people in the background, walking backwards",
+        help="Negative prompt for model generation.",
+    )
 
-dist.barrier()
-dist.destroy_process_group()
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    if args.weight_dtype == "fp32":
+        args.weight_dtype = torch.float32
+    elif args.weight_dtype == "fp16":
+        args.weight_dtype = torch.float16
+    else:
+        args.weight_dtype = torch.bfloat16
+
+    sp_group_index, sp_group_local_rank = init_sequence_parallel_group(8)
+
+    os.makedirs(args.output_path, exist_ok=True)
+    vae = AutoencoderKLWan.from_pretrained(
+        args.model_path,
+        subfolder="vae",
+        torch_dtype=torch.float32,
+    )
+    transformer = WanTransformer3DModel.from_pretrained(
+        args.model_path,
+        subfolder="transformer",
+        torch_dtype=args.weight_dtype,
+    )
+    transformer = replace_rmsnorm_with_fp32(transformer)
+
+    if args.lora_path is not None:
+        lora_target_modules = [
+            "to_q",
+            "to_k",
+            "to_v",
+            "to_out.0",
+            "proj_out",
+            "ffn.net.0.proj",
+            "ffn.net.2",
+        ]
+        lora_config = LoraConfig(r=64, lora_alpha=128, target_modules=lora_target_modules)
+        transformer = get_peft_model(transformer, lora_config)
+        state_dict = load_file(
+            args.lora_path,
+            device="cpu",
+        )
+        missing_keys, unexpected_keys = transformer.load_state_dict(state_dict, strict=False)
+        print("unexpected_keys: ", unexpected_keys)
+        print("loading lora done!")
+
+    transformer.eval()
+
+    if args.scheduler_type == "unipc":
+        scheduler = UniPCMultistepScheduler(
+            prediction_type="flow_prediction",
+            use_flow_sigmas=True,
+            num_train_timesteps=1000,
+            flow_shift=args.flow_shift,
+        )
+    else:
+        scheduler = FlowMatchEulerDiscreteScheduler(
+            num_train_timesteps=1000,
+            shift=args.flow_shift,
+        )
+
+    pipe = WanPipeline.from_pretrained(
+        args.model_path,
+        transformer=transformer,
+        vae=vae,
+        scheduler=scheduler,
+        torch_dtype=args.weight_dtype,
+    )
+
+    pipe = pipe.to(args.weight_dtype, device="cuda")
+
+    for prompt in tqdm(args.prompts):
+        generator = torch.Generator(device="cuda").manual_seed(args.seed)
+        with torch.amp.autocast("cuda", dtype=torch.bfloat16):
+            output = pipe(
+                prompt=prompt,
+                negative_prompt=args.negative_prompt,
+                height=args.height,
+                width=args.width,
+                num_inference_steps=args.num_inference_steps,
+                num_frames=args.num_frames,
+                guidance_scale=args.guidance_scale,
+                generator=generator,
+                is_flash_attn=True,
+            ).frames[0]
+        if dist.get_rank() == 0:
+            file_count = len(
+                [f for f in os.listdir(args.output_path) if os.path.isfile(os.path.join(args.output_path, f))]
+            )
+            video_path = f"{args.output_path}/{args.seed}_{file_count:04d}.mp4"
+            export_to_video(output, video_path, fps=16)
+
+    dist.barrier()
+    dist.destroy_process_group()
+
+
+if __name__ == "__main__":
+    main()
